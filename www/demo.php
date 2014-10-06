d00fa24832faa78a4c7f7288e600558d91e9ce95
Piotr "Hitori" Bosak <piotr.hitori.bosak@gmail.com>
2014-10-06 17:45:56 +0200

    split demo.php to html form and render example so one could easier see how to render pdf

--- www/demo.php
+++ www/demo.php
@@ -1,28 +1,9 @@
 <?php
 
-require_once("../dompdf_config.inc.php");
+require_once("render_example.php");
 
-// We check wether the user is accessing the demo locally
-$local = array("::1", "127.0.0.1");
-$is_local = in_array($_SERVER['REMOTE_ADDR'], $local);
-
-if ( isset( $_POST["html"] ) && $is_local ) {
-
-  if ( get_magic_quotes_gpc() )
-    $_POST["html"] = stripslashes($_POST["html"]);
-  
-  $dompdf = new DOMPDF();
-  $dompdf->load_html($_POST["html"]);
-  $dompdf->set_paper($_POST["paper"], $_POST["orientation"]);
-  $dompdf->render();
-
-  $dompdf->stream("dompdf_out.pdf", array("Attachment" => false));
-
-  exit(0);
-}
-
+include("head.inc");
 ?>
-<?php include("head.inc"); ?>
 
 <a name="demo"> </a>
 <h2>Demo</h2>
---
+++ www/render_example.php
@@ -0,0 +1,34 @@
+<?php
+
+// this is the simplest demo render script
+
+require_once("../dompdf_config.inc.php");
+
+// We check wether the user is accessing the demo locally
+$local = array("::1", "127.0.0.1");
+$is_local = in_array($_SERVER['REMOTE_ADDR'], $local);
+
+
+if ( isset( $_POST["html"] ) && $is_local ) {
+
+    $html = get_magic_quotes_gpc()?
+        stripslashes($_POST["html"])
+        :$_POST["html"];
+
+    $paper = isset($_POST["paper"])?
+        $_POST["paper"]
+        :'a4';
+
+    $orientation = isset($_POST["orientation"])?
+        $_POST["orientation"]
+        :'portrait';
+
+    $dompdf = new DOMPDF();
+    $dompdf->load_html($html);
+    $dompdf->set_paper($paper, $orientation);
+    $dompdf->render();
+
+    $dompdf->stream("dompdf_out.pdf", array("Attachment" => true));
+
+    exit(0);
+}
